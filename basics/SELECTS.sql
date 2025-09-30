fa84fe36b44d418e087ebe0dd40841d79343fc35
padmanabhakamath <padmanabha.kamath@gmail.com>
2025-09-30 12:25:56 -0500

    Basic Selects

---
+++ basics/SELECTS.sql
@@ -0,0 +1,12 @@
+-- Select 1000
+SELECT TOP (1000) *
+  FROM [WideWorldImporters].[Sales].[InvoiceLines]
+
+-- Select Distinct
+SELECT DISTINCT [StockItemID]
+  FROM [WideWorldImporters].[Sales].[InvoiceLines]
+
+-- Select LIMIT with distinct
+-- SQL Server does not support LIMIT, but you can use TOP with DISTINCT
+SELECT DISTINCT TOP (10) [StockItemID]
+    FROM [WideWorldImporters].[Sales].[InvoiceLines]
